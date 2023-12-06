ed7a50450542a63f838e331fd647efff0db34c53
Egoricetea <144737395+Egoricetea@users.noreply.github.com>
2023-12-06 20:49:41 +0300

    Add files via upload

---
+++ кр2/1 задание.pas
@@ -0,0 +1,23 @@
+﻿var 
+i,c,p:integer;
+a:array [1..10] of integer;
+n:array [1..80] of integer;
+begin
+for c:= 1 to 5 do 
+  read(a[c]);
+for c:= 1 to 80 do 
+  n[c]:=0;
+p:=1;
+for i:=1 to 5 do begin
+c:=1;
+while c <=5 do begin 
+if (a[c]+a[i]>a[c]*a[i]) and (c <> i) then begin
+n[p]:=a[i];
+n[p+1]:=a[c];
+p:=p+3;
+end;
+c:=c+1;
+end;
+end;
+writeln(n);
+end.
---
+++ кр2/2 задание.pas
@@ -0,0 +1,19 @@
+﻿var
+i:string;
+b,c,count,v:integer;
+
+begin
+  count:=0;
+  readln(i);
+  c:=Length(i);
+  for b:=1 to c do begin
+    if (i[b]<>' ') and (count = 0) then
+      count:= count+1;В
+    if (i[b]<>' ')  then
+      v:= v+1;
+    if (i[b]=' ') and (b+1<=c) and (i[b+1]<>' ') then
+      count:= count+1;
+  end;
+  writeln(count);
+  writeln(v);
+end.
